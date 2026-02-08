8c787fe29d5dc223773541fdd5ca945d4c517443
rajneesh1312 <32365894+rajneesh1312@users.noreply.github.com>
2026-02-08 12:52:07 +0530

    Add basic Express.js server setup

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
 const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-
 const app = express();
-app.use(cors());
-app.use(bodyParser.json());
+const PORT = process.env.PORT || 3000;
 
-const PORT = process.env.PORT || 5000;
-
-app.get('/api/products', (req, res) => {
-    res.send([]); // Dummy response to start
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
 });
 
 app.listen(PORT, () => {
